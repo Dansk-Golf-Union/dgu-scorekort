adf53320c8d57da6b9f44b7adc84e18e232e5b2c
nhuttel <nhuttel@users.noreply.github.com>
2025-12-05 22:22:56 +0000

    deploy: 129a71fe0ed875de1215549bdd5e78eba2b8274f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,9 +32,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "043a24c76b973085b8d0df56839041e5",
+"flutter_bootstrap.js": "fc12ba4f77dfe9079e8f82afd09ffde0",
 "version.json": "a523c48066a97a83f21370e4ae00a229",
-"main.dart.js": "0d8dea0e8b2732946ba000f2e820c3e6"};
+"main.dart.js": "6c7c640215542cff5605a821cc1fc98f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
--- main.dart.js
+++ main.dart.js
@@ -42240,12 +42240,12 @@ A.YY.prototype={
 XZ(){var s,r=$.aFi(),q=J.yU(64,t.N)
 for(s=0;s<64;++s)q[s]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~"[r.aok(66)]
 return B.b.uQ(q)},
-Y2(a){var s=t.N,r=A.al(["client_id","DGU_TEST_DK","redirect_uri",u.I,"response_type","code","scope","get_player.information none union","code_challenge",a,"code_challenge_method","S256"],s,s),q=A.k(r).i("dE<1,2>")
-return"https://auth.golfbox.io/authorize?"+A.p8(new A.dE(r,q),new A.YZ(),q.i("v.E"),s).bz(0,"&")},
+Y2(a){var s=t.N,r=A.al(["client_id","DGU_TEST_DK","redirect_uri",u.I,"response_type","code","scope","get_player.information none union","code_challenge",a,"code_challenge_method","S256","country_iso_code","dk"],s,s),q=A.k(r).i("dE<1,2>")
+return"https://auth.golfbox.io/connect/authorize?"+A.p8(new A.dE(r,q),new A.YZ(),q.i("v.E"),s).bz(0,"&")},
 z9(a,b){return this.akO(a,b)},
 akO(a,b){var s=0,r=A.N(t.N),q,p=2,o=[],n,m,l,k,j,i,h,g
 var $async$z9=A.O(function(c,d){if(c===1){o.push(d)
-s=p}for(;;)switch(s){case 0:h=A.ev("https://auth.golfbox.io/token",0,null)
+s=p}for(;;)switch(s){case 0:h=A.ev("https://auth.golfbox.io/connect/token",0,null)
 p=4
 k=t.N
 j=A.al(["Content-Type","application/x-www-form-urlencoded"],k,k)
